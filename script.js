a73eff0eb48fac3ae761444aa41d8511cadda6a0
Zulhimi Amar <zulhimiamar@gmail.com>
2022-01-12 08:43:20 +0800

    Clean up code

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ var chat = "?text=Hi%20I%20would%20like%20to%20enquire%20about%20your%20one%20of
 
 
 ita.onclick= function whatsApp(){
-    var whatsapp = window.open('https://wa.me/60123073646'+chat + selectedSubjectName(question) + pageInfo) ;
+    var whatsapp = window.open('https://wa.me/60123073646' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
  }
   
 thiru.onclick= function whatsApp2(){
-    var whatsapp2 = window.open('https://wa.me/60122646159?text=Hello%20I%20would%20like%20to%20enquire%20about%20your%20one%20of%20your%20programme '+ pageInfo) ;
+    var whatsapp2 = window.open('https://wa.me/60122646159' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
     }
 
 robert.onclick= function whatsApp3(){
-        var whatsapp3 = window.open('https://wa.me/60122022549?text=Hello%20I%20would%20like%20to%20enquire%20about%20your%20one%20of%20your%20programme '+ pageInfo) ;
+        var whatsapp3 = window.open('https://wa.me/60122022549' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
         }
     
     
