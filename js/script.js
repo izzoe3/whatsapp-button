6c6e30e7c3c8c3270c68d5d312a557a0ac297dca
izzoe3 <46365173+izzoe3@users.noreply.github.com>
2022-01-14 16:22:31 +0800

    Added comments on HTML and JS

--- js/script.js
+++ js/script.js
@@ -1,43 +1,28 @@
-// var ita= document.getElementById("ws-ita");
-var thiru= document.getElementById("ws-thiru");
-var robert= document.getElementById("ws-robert");
+
+//Custom Message to be added. Only replace message AFTER ?text=
+var chat = "?text=Hi I would like to know more about your product"
+//document.getElementbyID will get the text from based on ID we choose to be included in the message| document.URL will get the page url to be included in the message
 var pageInfo = encodeURIComponent(document.getElementById('header-title').textContent) + ' ' + encodeURIComponent(document.URL);
+//OPTIONAL: Function to output the text based on what's selected in the dropdown. Remove the whole thing if you didn't use it
 function selectedSubjectName(question) {
     var selectedText = question.options[question.selectedIndex].innerHTML;
     var selectedValue = question.value;
     return selectedValue
 }
 
-var chat = "?text=Hi%20I%20would%20like%20to%20enquire%20about%20your%20one%20of%20your%20programme "
 
- 
+//Multiple function if you have multiple number. Change the number accordingly. Remove whatsApp2 and whatsApp3 if you only need one
 
-
-// ita.onclick= function whatsApp(){
-//     var whatsapp = window.open('https://wa.me/60123073646' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
-//  }
-  
-// thiru.onclick= function whatsApp2(){
-//     var whatsapp2 = window.open('https://wa.me/60122646159' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
-//     }
-
-robert.onclick= function whatsApp3(){
+function whatsApp(){
+    
+    var whatsapp = window.open('https://wa.me/60123073646' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
+}
+function whatsApp2(){
+    var whatsapp2 = window.open('https://wa.me/60122646159' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
+    }
+function whatsApp3(){
         var whatsapp3 = window.open('https://wa.me/60122022549' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
         }
     
     
 
-
-function newWhatsapp(){
-    var x = document.querySelectorAll("#ws-ita, #ws-thiru");
-    if(x[0]){
-       
-        var whatsapp = window.open('https://wa.me/60123073646' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
-    }
-    else if(x[1]){
-        var whatsapp2 = window.open('https://wa.me/60122646159' +chat + selectedSubjectName(question) + ' ' + pageInfo) ;
-    }
-}
-
-document.getElementById("ws-ita").addEventListener("click", newWhatsapp);
-document.getElementById("ws-thiru").addEventListener("click", newWhatsapp);
